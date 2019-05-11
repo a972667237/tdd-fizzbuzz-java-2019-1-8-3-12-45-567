93fcbdf90649e2696feec792af5834808b58ae98
a972667237 <wuezmonster@gmail.com>
2019-05-12 03:44:39 +0800

    test: add tdd for fizzbuzz

--- src/main/java/com/wueiz/tdd/FizzBuzz.java
+++ src/main/java/com/wueiz/tdd/FizzBuzz.java
@@ -1,4 +1,13 @@
 package com.wueiz.tdd;
 
 public class FizzBuzz {
+
+    public int OneTwo;
+    public int OneThree;
+    public int OneTwoThree;
+    public int None;
+
+    public int Judge(int i) {
+        return i;
+    }
 }
--- src/test/java/com/wueiz/tdd/FizzBuzzTest.java
+++ src/test/java/com/wueiz/tdd/FizzBuzzTest.java
@@ -1,4 +1,32 @@
 package com.wueiz.tdd;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class FizzBuzzTest {
+    @Inject
+    private FizzBuzz fizzBuzz;
+
+    @BeforeAll
+    private void setUp() {
+
+    }
+
+    @Test
+    public void TestJudge() {
+        // Judge should return a int, mean that match 3, 5 and 7, if match 3, 5 ,return 4, define as OneTwo
+        int oneTwoValue = 15;
+        int oneThreeValue = 42;
+        int oneTwoThreeValue = 105;
+        int noneValue = 1;
+        assertEquals(fizzBuzz.Judge(oneTwoValue), fizzBuzz.OneTwo);
+        assertEquals(fizzBuzz.Judge(oneThreeValue), fizzBuzz.OneThree);
+        assertEquals(fizzBuzz.Judge(oneTwoThreeValue), fizzBuzz.OneTwoThree);
+        assertEquals(fizzBuzz.Judge(noneValue), fizzBuzz.None);
+    }
 }
